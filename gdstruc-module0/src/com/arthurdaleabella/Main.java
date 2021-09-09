dd0de82d7839596860d848ed3603256cbee63d56
LAPTOP-DVFIE2DV\Dale <arthurdale.abella@benilde.edu.ph>
2021-09-09 19:37:34 +0800

    quiz 1

---
+++ gdstruc-module0/src/com/arthurdaleabella/Main.java
@@ -0,0 +1,29 @@
+package com.arthurdaleabella;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] numbers = new int[5];
+
+        numbers[0] = 90;
+        numbers[1] = 85;
+        numbers[2] = 15;
+        numbers[3] = 50;
+        numbers[4] = 105;
+
+        int index = -1;
+
+        for (int i = 0; i < numbers.length; i++)
+        {
+            if (numbers[i] == 50)
+            {
+                index = i;
+                break;
+            }
+        }
+
+        System.out.println(index);
+
+    }
+}
